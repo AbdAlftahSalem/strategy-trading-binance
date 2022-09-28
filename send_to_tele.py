cf9bf7f70db08f059b908174e2a1fc0a30d31950
AbdAlftahSalem <abd321alftah@gmail.com>
2022-09-28 19:39:01 +0300

    delete const file

--- send_to_tele.py
+++ send_to_tele.py
@@ -1,9 +1,8 @@
 import requests
 
-import const
+# import const
 
 
-# https://api.telegram.org/bot5593961098:AAEfdla6P5VI18EElqhVtr9o_GxR5TsMyLI/sendMessage?chat_id=-1001573769581&text=hello
 def sentToTelegram(msg):
     sendMessage = 'https://api.telegram.org/bot' + const.token + '/sendMessage?chat_id=' + const.chatId1 + '&text=' + msg
     requests.get(sendMessage)
